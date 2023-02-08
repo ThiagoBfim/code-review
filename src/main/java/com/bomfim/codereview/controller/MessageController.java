56026a71a8ffaeead94d2c765736cddc8ee53510
Thiago Bomfim <thiagobomfim1995@gmail.com>
2023-02-08 17:45:05 +0000

    Including percentage problem

--- src/main/java/com/bomfim/codereview/controller/MessageController.java
+++ src/main/java/com/bomfim/codereview/controller/MessageController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bomfim.codereview.service.MessageService;
+
 @RestController
 @RequestMapping("/api/v1/Message")
 public class MessageController {
@@ -13,5 +15,10 @@ public class MessageController {
     @GetMapping
     public String showMessage(@RequestParam String message) {
         return message;
+    }
+
+    @GetMapping("/percentage")
+    public String messageResult(@RequestParam Double percentage) {
+        return new MessageService().getPercentageRounds(percentage);
     }
 }
