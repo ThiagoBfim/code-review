1cfb30f3782205efa6048c1a2dc68e02f58ff74b
Thiago Bomfim <thiagobomfim1995@gmail.com>
2023-02-08 17:57:19 +0000

    Including security

--- src/main/java/com/bomfim/codereview/controller/MessageController.java
+++ src/main/java/com/bomfim/codereview/controller/MessageController.java
@@ -1,6 +1,8 @@
 package com.bomfim.codereview.controller;
 
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +15,13 @@ import com.bomfim.codereview.service.MessageService;
 public class MessageController {
 
     @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public String showMessage(@RequestParam String message) {
         return message;
     }
 
     @GetMapping("/percentage")
+    @Secured("ROLE_ADMIN") /*Let's assume we have this role in our project*/
     public String messageResult(@RequestParam Double percentage) {
         return new MessageService().getPercentageRounds(percentage);
     }
