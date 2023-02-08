fe464a7f94a83042be6c252b188287d92c1089e2
Thiago Bomfim <thiagobomfim1995@gmail.com>
2023-02-08 17:38:25 +0000

    Creating message endpoint

--- src/main/java/com/bomfim/codereview/controller/MessageController.java
+++ src/main/java/com/bomfim/codereview/controller/MessageController.java
@@ -1,2 +1,17 @@
-package com.bomfim.codereview.controller;public class MessageController {
+package com.bomfim.codereview.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/Message")
+public class MessageController {
+
+    @GetMapping
+    public String showMessage(@RequestParam String message) {
+        return message;
+    }
 }
