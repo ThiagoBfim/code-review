bffad5ff5dc63df178c9e0c71d1daa0db85417fe
Thiago Bomfim <thiagobomfim1995@gmail.com>
2023-02-09 11:48:41 +0000

    Including persistence message

--- src/main/java/com/bomfim/codereview/controller/MessageController.java
+++ src/main/java/com/bomfim/codereview/controller/MessageController.java
@@ -1,9 +1,14 @@
 package com.bomfim.codereview.controller;
 
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +29,17 @@ public class MessageController {
     @Secured("ROLE_ADMIN") /*Let's assume we have this role in our project*/
     public String messageResult(@RequestParam Double percentage) {
         return new MessageService().getPercentageRounds(percentage);
+    }
+
+    @PostMapping
+    public String saveNewMessage(@RequestParam String message) {
+        final File file = new File("message.txt");
+        try {
+            Files.writeString(file.toPath(), message);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return message;
     }
 }
